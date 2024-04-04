48f5a7c71078adf9e6eb92a5e64c5b1f5cafb45b
piidus <sudiipkumarbasu@gmail.com>
2024-04-04 14:50:09 +0530

    live and main

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 
 """
-Script for managing hot reloading of the project.
-For more details see the documentation page -
-
-https://kivymd.readthedocs.io/en/latest/api/kivymd/tools/patterns/create_project/
-
-To run the application in hot boot mode, execute the command in the console:
-DEBUG=1 python main.py
+Script for run for final
 """
 
-import importlib
+# import importlib
 import os
 
-from kivy import Config
-from kivymd.theming import ThemeManager
+# from kivy import Config
+# from kivymd.theming import ThemeManager
 # from PIL import ImageGrab
 
-# TODO: You may know an easier way to get the size of a computer display.
-Config.set("graphics", "height", '600')
-Config.set("graphics", "width", "400")
+# # TODO: You may know an easier way to get the size of a computer display.
+# Config.set("graphics", "height", 900)
+# Config.set("graphics", "width", 300)
 
-from kivy.core.window import Window
+# from kivy.core.window import Window
 
 # Place the application window on the right side of the computer screen.
-# Window.top = 0
-Window.left =  Window.width - 450
+# Window.top = 30
+# Window.right= 10 #=  Window.width - 650
 from kivymd.tools.hotreload.app import MDApp
 from kivymd.uix.screenmanager import MDScreenManager
 
 
 class MYProject(MDApp):
     KV_DIRS = [os.path.join(os.getcwd(), "View")]
+    
 
     def build_app(self) -> MDScreenManager:
         """
@@ -44,8 +39,6 @@ class MYProject(MDApp):
         self.manager_screens = MDScreenManager()
         
 
-        Window.bind(on_key_down=self.on_keyboard_down)
-        importlib.reload(View.screens)
         screens = View.screens.screens
 
         for i, name_screen in enumerate(screens.keys()):
@@ -58,71 +51,6 @@ class MYProject(MDApp):
 
         return self.manager_screens
 
-    def on_keyboard_down(self, window, keyboard, keycode, text, modifiers) -> None:
-        """
-        The method handles keyboard events.
-
-        By default, a forced restart of an application is tied to the
-        `CTRL+R` key on Windows OS and `COMMAND+R` on Mac OS.
-        """
-
-        if "meta" in modifiers or "ctrl" in modifiers and text == "r":
-            self.rebuild()
-
-
-MYProject().run()
-
-# After you finish the project, remove the above code and uncomment the below
-# code to test the application normally without hot reloading.
-
-# """
-# The entry point to the application.
-# 
-# The application uses the MVC template. Adhering to the principles of clean
-# architecture means ensuring that your application is easy to test, maintain,
-# and modernize.
-# 
-# You can read more about this template at the links below:
-# 
-# https://github.com/HeaTTheatR/LoginAppMVC
-# https://en.wikipedia.org/wiki/Model–view–controller
-# """
-# 
-# from kivymd.app import MDApp
-# from kivymd.uix.screenmanager import MDScreenManager
-# 
-# from View.screens import screens
-# 
-# 
-# class MYProject(MDApp):
-#     def __init__(self, **kwargs):
-#         super().__init__(**kwargs)
-#         self.load_all_kv_files(self.directory)
-#         # This is the screen manager that will contain all the screens of your
-#         # application.
-#         self.manager_screens = MDScreenManager()
-#         
-#     def build(self) -> MDScreenManager:
-#         self.generate_application_screens()
-#         return self.manager_screens
-# 
-#     def generate_application_screens(self) -> None:
-#         """
-#         Creating and adding screens to the screen manager.
-#         You should not change this cycle unnecessarily. He is self-sufficient.
-# 
-#         If you need to add any screen, open the `View.screens.py` module and
-#         see how new screens are added according to the given application
-#         architecture.
-#         """
-# 
-#         for i, name_screen in enumerate(screens.keys()):
-#             model = screens[name_screen]["model"]()
-#             controller = screens[name_screen]["controller"](model)
-#             view = controller.get_view()
-#             view.manager_screens = self.manager_screens
-#             view.name = name_screen
-#             self.manager_screens.add_widget(view)
-# 
-# 
-# MYProject().run()
+   
+if __name__ == "__main__":
+    MYProject().run()
